48f1436a6cb264b41a6f989cd322a127472dfff3
williamvelichko <williamvelichko2003@gmail.com>
2022-01-20 13:20:10 -0800

    almost finished with all the context api

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+import MainContext from "./contexts/mainContexts";
+import CartContext from "./contexts/CartContext";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = (item) => {
+    // add the given item to the cart
+    console.log(item);
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <div className="App">
+      <MainContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={cart}>
+          <Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </CartContext.Provider>
+        {/* Routes */}
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+        <Route exact path="/">
+          <Products />
+        </Route>
+      </MainContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useContext } from "react";
+import MainContext from "../contexts/mainContexts";
 
 // Components
-import Product from './Product';
+import Product from "./Product";
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+const Products = () => {
+  const products = useContext(MainContext);
+  return (
+    <div className="products-container">
+      {products.products.map((product) => (
+        <Product
+          key={product.id}
+          product={product}
+          addItem={products.addItem}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
