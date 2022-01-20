1e872657a7a0a7d052d69d0c18e42c5b5f600bac
williamvelichko <williamvelichko2003@gmail.com>
2022-01-20 14:06:56 -0800

    finished all the requirments

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import CartContext from "../contexts/CartContext";
 
-const Item = props => {
-	return (
-		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+const Item = (props) => {
+  console.log(props);
+  // const item = useContext(CartContext);
+  const handleDelete = (item) => {
+    console.log(item);
+  };
+  return (
+    <div className="shopping-cart_item">
+      <img src={props.image} alt={`${props.title} book`} />
 
-
-			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
-			</div>
-		</div>
-	);
+      <div>
+        <h1>{props.title}</h1>
+        <p>$ {props.price}</p>
+        <button onClick={() => handleDelete(props)}>Remove from cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
